acf90cc5feb4cdd649e3fe61105bdb46292df000
Memousek <namiskoczech@gmail.com>
2025-08-17 16:21:06 +0200

    feat: add custom headers for caching in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
     ],
   },
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
